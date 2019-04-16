5e425b66a43d5c647a9037592e2844c9222e79fc
umarmuha <21365827+umarmuha@users.noreply.github.com>
2019-04-16 12:54:38 -0500

    This commit was to beatify code

--- gettersAndSetters/getset.js
+++ gettersAndSetters/getset.js
@@ -15,7 +15,7 @@
 //     set c(c){
 //         this._c = c;
 //     }
-    
+
 // };
 
 // console.log(thing.a);
@@ -28,23 +28,23 @@
 // The example above I used the backing property. 
 // I want to test if I dont set the name of the property and the passed in Parameter the same and then I DONT have to use a backing property 
 
-    
+
 
 let thing = {
-    a : 7,
+    a: 7,
 
-    get b(){
+    get b() {
         return (this.a + 1);
     },
 
-    get c(){
+    get c() {
         return this.a = (this._c / 2);
     },
 
-    set c(x){
-        this._c = x;  // The key is that the name of the property this._c can never be the same as the name of the setter "c" in this case
+    set c(x) {
+        this._c = x; // The key is that the name of the property this._c can never be the same as the name of the setter "c" in this case
     }
-    
+
 };
 
 console.log(thing.a);
@@ -53,9 +53,11 @@ console.log(thing.b);
 thing.c = 50;
 console.log(thing.c);
 
-``` 
+``
+` 
     Conclusion: is that the name of the property example this._c can not be the same as the setter method "c" in our example
     It cannot be this.c for setter function c. Even if the value being passed to the setter function has the same name or 
     not, it does not matter so setc(c){this._c = c}; and setc(x){this._c = x}; are the same. The only thing matters is the 
     name of the backing property and the name of the setter function. 
-```
+`
+``
