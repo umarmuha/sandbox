97e706b09079253723f024e1b9e5a21958623826
umarmuha <21365827+umarmuha@users.noreply.github.com>
2019-04-25 10:15:13 -0500

    This commit will add practice object interaction module

---
+++ Practice Object Interaction/README.md
@@ -0,0 +1,9 @@
+Hello!
+
+This README file contains instructions for video "A Rough Plan", S1V2.
+
+Your instructions are as follows:
+
+1) In the Library.js file, declare a Library class. Do not yet add a constructor method.
+2) In the Book.js file, declare a Book class. Do not yet add a constructor method.
+3) In the Patron.js file, declare a Patron class. Do not yet add a constructor method.
---
+++ test.js
@@ -0,0 +1,35 @@
+a = [{
+    firstName: "John",
+    lastName: "Bravo"
+}, {
+    firstName: "Pam",
+    lastName: "Fix"
+}, {
+    firstName: "Tom",
+    lastName: "Pilaf"
+}];
+
+b = {
+    firstName: "John",
+    lastName: "Bravo"
+};
+
+
+
+// for (const item of a) {
+//     if (item.firstName === b.firstName) {
+//       console.log("Hurray");  
+//     } 
+//     else {
+//         console.log("not good")
+//     }
+// }
+
+//console.log(a.includes(b.firstName));
+
+
+fruit = ["apple", "banana"];
+
+console.log(fruit.includes("banana"))
+
+//console.log(a[0]);
