5cce20f41753ff437a8db66d36e65586b5119828
umarmuha <21365827+umarmuha@users.noreply.github.com>
2019-09-05 21:04:40 -0500

    This commit will add the reverse array exercise (new array and inline array change) with comments explaining the solution and approach

--- eloquentJavaScript/Chapter4/reversingAnArray.js
+++ eloquentJavaScript/Chapter4/reversingAnArray.js
@@ -6,12 +6,24 @@
  * Neither may use the standard reverse method.
  */
 
- function reverseArray(array) {
-     let reverse = [];
-     for (let index = (array.length - 1); index >= 0; index--) {
-         reverse.push(array[index]);
-     }
-     return reverse;
- }
+function reverseArray(array) {
+    let reverse = [];
+    for (let index = (array.length - 1); index >= 0; index--) {
+        reverse.push(array[index]);
+    }
+    return reverse;
+}
 
- console.log(reverseArray([1,2,3,4]));
+console.log(reverseArray([1, 2, 3, 4]));
+
+
+function reverseInPlace(array) {
+    for (let index = 0; index < Math.floor(array.length / 2)  ; index++) { //Using divided by 2 to go all the way till the half of the array.
+        let old = array[index]; //Since this index value gets replaced, we are saving it in a variable for assigning it in later step
+        array[index] = array[array.length - 1 - index]; //This subtraction will decrement the index from right to left so a[0] = a[4-1-0], so a[0] = a[3]
+        array[array.length -1 -index] = old; //Since a[index] is replaced by the a[a.length-1-index] value, its value is replaced by the old value which held a[index]
+    }
+    return array;
+}
+
+console.log(reverseInPlace([1, 2, 3, 4]));
